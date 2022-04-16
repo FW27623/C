478bb90435e57ad471870b29405ffda3af39e002
FangWei <1970446279@qq.com>
2022-04-16 15:41:14 +0800

    增加注释

--- LoveYou.c
+++ LoveYou.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 void LoveYou(int n)//n为问题规模
 {
-    int i=1;
-    while(i<=n)
+    int i=1;//①--1次
+    while(i<=n)//②--3001次
     {
-        printf("I Love You\n",i);
-        i++;
+        printf("I Love You\n",i);//③--3000次
+        i++;//④--3000次
     }
-    printf("I Love You More than %d\n",n);
+    printf("I Love You More than %d\n",n);//⑤--1次
 }
+//T*(3000)=1+3001+2*3000+1
+//T*(n)=3n+3
 int main()
 {
     LoveYou(3000);
