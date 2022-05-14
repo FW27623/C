038a637b188947247cd6fa8c1d811db425ea93f9
FangWei <1970446279@qq.com>
2022-05-14 20:32:00 +0800

    增加注释

--- calc.c
+++ calc.c
@@ -1,11 +1,13 @@
+//#define _CRT_SECURE_NO_WARNINGS//解决vs编译器不支持的警告
 #include<stdio.h>
 int main(){
     int a = 0;
     int b = 0;
     int c = 0;
     printf("Enter a number: ");
-    scanf("%d" "%d", &a,&b); 
+    scanf("%d%d", &a,&b); 
     c = a + b;
-    printf("%d+%d=%d",a,b,c);
+    printf("%d+%d=%d\n",a,b,c);
+    printf("%d",a+b);
     return 0;
 }
